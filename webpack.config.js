e0872005e46774cffe5f77943ce6adc59a84174d
Jeanette Skoog <jeanetteskoog@n153-p195.eduroam.kth.se>
2019-03-26 13:51:16 +0100

    css added in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
 module.exports = {
-  entry: {
-    app: './public/js/app/app.jsx'
-  },
+  entry:
+    [ './public/js/app/app.jsx', './public/css/node-web.scss' ],
   output: {
-    path: path.resolve(__dirname, 'dist/js/'),
-    filename: '[name].js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
   },
   module: {
     rules: [
-      { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader' }
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin('css/node-web.css')
+  ],
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx', '.css']
   }
 }
