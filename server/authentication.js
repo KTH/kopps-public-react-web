213e5196ed2c9144f1e6ead435a22963c6028c1e
Viktor Atterlönn <viktor_atterlonn@live.se>
2019-03-28 17:01:08 +0100

    Refactoring due to complaints from eslint

--- server/authentication.js
+++ server/authentication.js
@@ -4,7 +4,7 @@ const passport = require('passport')
 const config = require('./configuration').server
 const log = require('kth-node-log')
 const CasStrategy = require('kth-node-passport-cas').Strategy
-const GatewayStrategy = require('kth-node-passport-cas').GatewayStrategy
+const { GatewayStrategy } = require('kth-node-passport-cas')
 
 /**
  * Passport will maintain persistent login sessions. In order for persistent sessions to work, the authenticated
@@ -14,7 +14,7 @@ const GatewayStrategy = require('kth-node-passport-cas').GatewayStrategy
  * to Passport which implements the necessary serialization and deserialization logic. In a typical
  * application, this will be as simple as serializing the user ID, and finding the user by ID when deserializing.
  */
-passport.serializeUser(function (user, done) {
+passport.serializeUser((user, done) => {
   if (user) {
     log.debug('User serialized: ' + user)
     done(null, user)
@@ -23,7 +23,7 @@ passport.serializeUser(function (user, done) {
   }
 })
 
-passport.deserializeUser(function (user, done) {
+passport.deserializeUser((user, done) => {
   if (user) {
     log.debug('User deserialized: ' + user)
     done(null, user)
@@ -43,50 +43,55 @@ passport.deserializeUser(function (user, done) {
 const casOptions = {
   ssoBaseURL: config.cas.ssoBaseURL,
   serverBaseURL: config.hostUrl,
-  log: log
+  log
 }
 
 if (config.cas.pgtUrl) {
   casOptions.pgtURL = config.hostUrl + config.cas.pgtUrl
 }
 
-const strategy = new CasStrategy(casOptions,
-  function (logOnResult, done) {
-    const user = logOnResult.user
-    log.debug(`User from CAS: ${user} ${JSON.stringify(logOnResult)}`)
-    return done(null, user, logOnResult)
-  }
-)
+const strategy = new CasStrategy(casOptions, (logOnResult, done) => {
+  const { user } = logOnResult
+  log.debug(`User from CAS: ${user} ${JSON.stringify(logOnResult)}`)
+  return done(null, user, logOnResult)
+})
 
 passport.use(strategy)
 
-passport.use(new GatewayStrategy({
-  casUrl: config.cas.ssoBaseURL
-}, function (result, done) {
-  log.debug({ result: result }, `CAS Gateway user: ${result.user}`)
-  done(null, result.user, result)
-}))
+passport.use(
+  new GatewayStrategy(
+    {
+      casUrl: config.cas.ssoBaseURL
+    },
+    (result, done) => {
+      log.debug({ result }, `CAS Gateway user: ${result.user}`)
+      done(null, result.user, result)
+    }
+  )
+)
 
 // The factory routeHandlers.getRedirectAuthenticatedUser returns a middleware that sets the user in req.session.authUser and
 // redirects to appropriate place when returning from CAS login
 // The unpackLdapUser function transforms an ldap user to a user object that is stored as
 const ldapClient = require('./adldapClient')
 const { hasGroup } = require('kth-node-ldap').utils
-module.exports.redirectAuthenticatedUserHandler = require('kth-node-passport-cas').routeHandlers.getRedirectAuthenticatedUser({
-  ldapConfig: config.ldap,
-  ldapClient: ldapClient,
-  proxyPrefixPath: config.proxyPrefixPath.uri,
-  unpackLdapUser: function (ldapUser, pgtIou) {
-    return {
-      username: ldapUser.ugUsername,
-      displayName: ldapUser.displayName,
-      email: ldapUser.mail,
-      pgtIou: pgtIou,
-      // This is where you can set custom roles
-      isAdmin: hasGroup(config.auth.adminGroup, ldapUser)
+module.exports.redirectAuthenticatedUserHandler = require('kth-node-passport-cas').routeHandlers.getRedirectAuthenticatedUser(
+  {
+    ldapConfig: config.ldap,
+    ldapClient,
+    proxyPrefixPath: config.proxyPrefixPath.uri,
+    unpackLdapUser: (ldapUser, pgtIou) => {
+      return {
+        username: ldapUser.ugUsername,
+        displayName: ldapUser.displayName,
+        email: ldapUser.mail,
+        pgtIou,
+        // This is where you can set custom roles
+        isAdmin: hasGroup(config.auth.adminGroup, ldapUser)
+      }
     }
   }
-})
+)
 
 /*
   Checks req.session.authUser as created above im unpackLdapUser.
@@ -96,14 +101,17 @@ module.exports.redirectAuthenticatedUserHandler = require('kth-node-passport-cas
   requireRole('isAdmin', 'isEditor')
 */
 
-module.exports.requireRole = function () {
+function requireRole() {
   const roles = Array.prototype.slice.call(arguments)
 
-  return function _hasNoneOfAcceptedRoles (req, res, next) {
+  return function _hasNoneOfAcceptedRoles(req, res, next) {
     const ldapUser = req.session.authUser || {}
 
     // Check if we have any of the roles passed
-    const hasAuthorizedRole = roles.reduce((prev, curr) => prev || ldapUser[curr], false)
+    const hasAuthorizedRole = roles.reduce(
+      (prev, curr) => prev || ldapUser[curr],
+      false
+    )
     // If we don't have one of these then access is forbidden
     if (!hasAuthorizedRole) {
       const error = new Error('Forbidden')
@@ -113,3 +121,5 @@ module.exports.requireRole = function () {
     return next()
   }
 }
+
+module.exports = { requireRole }
--- server/configuration.js
+++ server/configuration.js
@@ -1,4 +1,5 @@
 'use strict'
+
 const { generateConfig } = require('kth-node-configuration')
 
 // These settings are used by the server
