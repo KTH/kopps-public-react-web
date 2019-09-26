7348152f6cc68d6f1f2f6fe07b214ca1ec74240d
Viktor Atterlönn <viktor_atterlonn@live.se>
2019-09-26 16:47:16 +0200

    Added .prettierrc file

--- test/unit/test-sampleCtrl.js
+++ test/unit/test-sampleCtrl.js
@@ -2,53 +2,53 @@
 /* eslint-env mocha */
 /* eslint-disable no-unused-expressions */
 
-process.env.LDAP_URI = "ldaps://mockuser@mockdomain.com@mockldapdomain.com";
-process.env.LDAP_PASSWORD = "mockldappassword";
+process.env.LDAP_URI = 'ldaps://mockuser@mockdomain.com@mockldapdomain.com'
+process.env.LDAP_PASSWORD = 'mockldappassword'
 
-const { expect } = require("chai");
-const nock = require("nock");
-const mockery = require("mockery");
-const httpMocks = require("node-mocks-http");
+const { expect } = require('chai')
+const nock = require('nock')
+const mockery = require('mockery')
+const httpMocks = require('node-mocks-http')
 
-const mockLogger = {};
-mockLogger.debug = mockLogger.info = mockLogger.error = mockLogger.warn = () => {};
-mockLogger.init = () => {};
+const mockLogger = {}
+mockLogger.debug = mockLogger.info = mockLogger.error = mockLogger.warn = () => {}
+mockLogger.init = () => {}
 
-mockery.registerMock("kth-node-log", mockLogger);
+mockery.registerMock('kth-node-log', mockLogger)
 mockery.enable({
   warnOnReplace: false,
   warnOnUnregistered: false
-});
+})
 
-const paths = require("../mocks/apipaths.json");
-const api = nock("http://localhost:3001/api/node")
-  .get("/_paths")
+const paths = require('../mocks/apipaths.json')
+const api = nock('http://localhost:3001/api/node')
+  .get('/_paths')
   .reply(200, paths)
-  .get("/_checkAPIkey")
-  .reply(200, {});
+  .get('/_checkAPIkey')
+  .reply(200, {})
 
-describe("Index page", () => {
+describe('Index page', () => {
   before(done => {
-    require("../../server/api");
+    require('../../server/api')
     setTimeout(() => {
-      done();
-    }, 500);
-  });
+      done()
+    }, 500)
+  })
 
-  it("should get the index page", done => {
-    api.get("/v1/data/123").reply(200, {
-      id: "123",
-      name: "asdasd"
-    });
+  it('should get the index page', done => {
+    api.get('/v1/data/123').reply(200, {
+      id: '123',
+      name: 'asdasd'
+    })
 
-    const ctrl = require("../../server/controllers/sampleCtrl");
-    const { req, res } = httpMocks.createMocks();
+    const ctrl = require('../../server/controllers/sampleCtrl')
+    const { req, res } = httpMocks.createMocks()
 
     res.render = (view, data) => {
-      expect(data).to.be.not.undefined;
-      done();
-    };
+      expect(data).to.be.not.undefined
+      done()
+    }
 
-    ctrl.getIndex(req, res, () => {});
-  });
-});
+    ctrl.getIndex(req, res, () => {})
+  })
+})
