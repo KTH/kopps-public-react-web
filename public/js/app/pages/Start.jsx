21704d27911e62c1300192d2b8bd1b95d05733d3
Niklas Olsson <hoyce@kth.se>
2019-11-20 14:21:01 +0100

    Updated packages and removed vulnerabilites

--- public/js/app/pages/Start.jsx
+++ public/js/app/pages/Start.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { inject, observer } from 'mobx-react'
-import { Jumbotron, Button } from 'reactstrap'
 
 @inject(['routerStore'])
 @observer
 class Start extends Component {
   state = {
-    buttonClicked: false
+    buttonClicked: false,
   }
 
   toggleButton = () => this.setState({ buttonClicked: !this.state.buttonClicked })
@@ -14,16 +13,16 @@ class Start extends Component {
   render() {
     const { message } = this.props.routerStore
     return (
-      <div>
-        <Jumbotron>
-          <h1 className="display-3">Node-web</h1>
-          <h2>You are upp and running kth-node react!</h2>
-          <hr className="my-2" />
-          <h3>{`Message from routerStore: ${message}`}</h3>
-          <Button onClick={this.toggleButton}>Try me</Button>
-          <p>{this.state.buttonClicked ? 'Button works!' : ''}</p>
-        </Jumbotron>
-      </div>
+      <Fragment>
+        <h1 className="display-3">Node-web</h1>
+        <h2>You are upp and running kth-node react!</h2>
+        <hr className="my-2" />
+        <h3>{`Message from routerStore: ${message}`}</h3>
+        <button type="button" onClick={this.toggleButton}>
+          Try me
+        </button>
+        <p>{this.state.buttonClicked ? 'Button works!' : ''}</p>
+      </Fragment>
     )
   }
 }
