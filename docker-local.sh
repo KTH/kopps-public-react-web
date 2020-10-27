0dac3cd1eba76499f1c0644ff28541d6c4aa9e61
Alexander Urban <lexa@toxa.de>
2020-10-27 18:20:47 +0100

    Basic setup revised: docker:pre-push

--- docker-local.sh
+++ docker-local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# File version tag: 2020-10-21
+# File version tag: 2020-10-27
 
 CMD=$1
 
@@ -19,6 +19,9 @@ function main() {
   elif [ "$CMD" == "clean" ]; then
     clean
     printf "\nDone."
+  elif [ "$CMD" == "pre-push" ]; then
+    prepush
+    printf "\nDone."
   else
     usage
   fi
@@ -33,19 +36,23 @@ function start() {
     error "Invalid project-tag"
   fi
 
-  echoYellow "--> Building Docker image of $PROJECTNAME <--\n" 1
+  echo
+  echoYellow "--> Building Docker image of $PROJECTNAME <--\n"
   docker build --tag $PROJECTTAG . || exit 1
 
-  echoYellow "--> Docker image of $PROJECTNAME ready <--\n" 1
+  echo
+  echoYellow "--> Docker image of $PROJECTNAME ready <--\n"
   docker images $PROJECTTAG || exit 1
 
   if [ -f docker-local.env ]; then
-    echoYellow "--> Starting new Docker container with $PROJECTNAME <--" 1
+    echo
+    echoYellow "--> Starting new Docker container with $PROJECTNAME <--"
     echoYellow "   (Using port $PORT and file 'docker-local.env')"
     echoYellow "  Remember 'npm run docker:stop' when you are done!\n"
     docker run --publish $PORT:$PORT --env-file docker-local.env $PROJECTTAG || exit 1
   else
-    echoYellow "--> Starting new Docker container with $PROJECTNAME <--" 1
+    echo
+    echoYellow "--> Starting new Docker container with $PROJECTNAME <--"
     echoYellow "   (Using port $PORT and file '.env')"
     echoYellow "  Remember 'npm run docker:stop' when you are done!\n"
     docker run --publish $PORT:$PORT --env-file .env $PROJECTTAG || exit 1
@@ -55,13 +62,15 @@ function start() {
 function stop() {
   IDS1=$(docker ps --filter ancestor=$PROJECTTAG --all --quiet)
   if [ -n "$IDS1" ]; then
-    echoYellow "--> Stopping any Docker container of $PROJECTNAME <--\n" 1
+    echo
+    echoYellow "--> Stopping any Docker container of $PROJECTNAME <--\n"
     docker rm --volumes --force $(echo "$IDS1") || exit 1
   fi
 
   IDS2=$(docker ps --filter status=exited --all --quiet)
   if [ -n "$IDS2" ]; then
-    echoYellow "--> Removing any stopped Docker container <--\n" 1
+    echo
+    echoYellow "--> Removing any stopped Docker container <--\n"
     docker rm --volumes $(echo "$IDS2") || exit 1
   fi
 }
@@ -71,8 +80,28 @@ function clean() {
 
   IDS3=$(docker images --quiet $PROJECTTAG)
   if [ -n "$IDS3" ]; then
-    echoYellow "--> Deleting Docker images of $PROJECTNAME <--\n" 1
+    echo
+    echoYellow "--> Deleting Docker images of $PROJECTNAME <--\n"
     docker rmi --force $(echo "$IDS3") || exit 1
+  fi
+}
+
+function prepush() {
+  if [ -r .env ]; then
+    source .env 2> /dev/null
+  elif [ -r docker-local.env ]; then
+    source docker-local.env 2> /dev/null
+  fi
+
+  if [ "$RUN_DOCKER_BUILD_BEFORE_GIT_PUSH" == "true" ]; then
+    echo
+    echoYellow "--> Testing build of Docker image of $PROJECTNAME <--\n"
+    docker build --tag $PROJECTTAG . || exit 1
+  else
+    echo
+    echo "--> NOT testing build of Docker image <--"
+    echo " Add the following in .env or docker-local.env to activate this step:"
+    echo "    RUN_DOCKER_BUILD_BEFORE_GIT_PUSH=true"
   fi
 }
 
@@ -82,7 +111,7 @@ function usage() {
   echo "Usage:"
   echo "  $0 <command> <project-tag?>"
   echo
-  echo "  where <command>      is 'start', 'stop' or 'clean',"
+  echo "  where <command>      is 'start', 'stop', 'clean' or 'pre-push',"
   echo "        <project-tag?> is a project name that ends with '-api' or '-web'"
   echo "                       (or IMAGE_FILE from docker.conf will be used, if omitted)"
   echo
@@ -100,10 +129,6 @@ function error() {
 
 function echoYellow() {
   MSG=$1
-  NUMBER_OF_LINES_ABOVE_AVOIDING_SOME_PREFIX_DISABLES_COLORS=$2
-  for (( I=${NUMBER_OF_LINES_ABOVE_AVOIDING_SOME_PREFIX_DISABLES_COLORS:-0}; I>0; I-- )); do
-    echo
-  done
   printf "\033[1;33m$MSG\033[0m\n"
 }
 
