e33e50c40a8ca4a52306ee15a38f0018a2552432
Carl Frendin <cfrendin@kth.se>
2021-03-04 18:05:55 +0100

    KP-115 feature: poc for embedded fovsearch kinda works

--- server/controllers/embeddedPagesCtrl.js
+++ server/controllers/embeddedPagesCtrl.js
@@ -3,28 +3,52 @@
 const { searchFovCourses } = require('../koppsApi/setupKoppsApi')
 const language = require('kth-node-web-common/lib/language')
 
+function _convertUserOptionsToKoppsApiParams({ type, start }) {
+  const koppsApiParams = {
+    category: 'VU',
+    exludedTypes: 'SAP',
+    lang: 'sv', // TODO: use lang from user of embedded html api.
+  }
+  if (!start || start === 'current') {
+    koppsApiParams.date = new Date().toISOString() // TODO: not accurate to current KP behaviour.
+  } else {
+    koppsApiParams.term = parseInt(start)
+  }
+  switch (type) {
+    case 'ALL':
+    default:
+      break
+    case 'DISTANCE':
+      koppsApiParams.tutoringForm = 'DST'
+      break
+    case 'ENGLISH':
+      koppsApiParams.inEnglish = true
+      break
+    case 'EVENING':
+      koppsApiParams.tutoringTime = 'KVÄ'
+      break
+    case 'ITDISTANCE':
+      koppsApiParams.tutoringForm = 'ITD'
+      break
+    case 'SUMMER':
+      koppsApiParams.category = null
+      koppsApiParams.types = asList('SFV', 'SN\u00C4')
+      break
+    case 'TEACHER':
+      koppsApiParams.teachersFurtherEdu = true
+      break
+  }
+
+  return koppsApiParams
+}
+
 async function _fovSearch(req, res, next) {
   const lang = language.getLanguage(res)
-  const paramString = '' //TODO: hard coded for testing. Need to read query parameters and remap them to kopps api parameters.
-  let searchOptions
-  if (paramString) {
-    searchOptions = paramString
-      .split('&')
-      .map(paramPairString => {
-        const [, key, value] = /([a-zA-Z]+)=([a-zA-Z]+)/.exec(paramPairString)
-        return { key, value }
-      })
-      .reduce((currentParams, { key, value }) => {
-        currentParams[key] = value
-        return currentParams
-      }, {})
-  } else {
-    searchOptions = {}
-  }
-  const fovCoursesResults = await searchFovCourses(searchOptions)
+  const fovCoursesResults = await searchFovCourses(_convertUserOptionsToKoppsApiParams(req.query))
   res.render('embedded/fovsearch', {
-    title: 'TODO',
-    description: 'TODO',
+    layout: false, // No kth menu, no breadcrumbs, etc.
+    title: 'fovsearch',
+    description: '',
     breadcrumbsPath: [],
     lang,
     fovCoursesResults,
