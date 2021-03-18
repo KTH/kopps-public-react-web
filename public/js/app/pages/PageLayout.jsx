fad80c6d6bb06cda2944436f046511f1c312b491
Rickard Falk <rickard.falk@gmail.com>
2021-03-18 14:54:55 +0100

    feat: Use Breadcrumbs component

--- public/js/app/pages/PageLayout.jsx
+++ public/js/app/pages/PageLayout.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import ReactDOM from 'react-dom'
 import { Col, Row } from 'reactstrap'
+import { Breadcrumbs } from '@kth/kth-reactstrap/dist/components/utbildningsinfo'
 import MainMenu from '../components/MainMenu'
+import { useStore } from '../mobx'
+
+function renderBreadcrumbsIntoKthHeader(language) {
+  const breadcrumbContainer = document.getElementById('breadcrumbs-header')
+  if (breadcrumbContainer) {
+    ReactDOM.render(<Breadcrumbs include="directory" language={language} />, breadcrumbContainer)
+  }
+}
 
 function MainContent({ children }) {
   return <main id="mainContent">{children}</main>
 }
 
 function PageLayout({ children }) {
+  const { language } = useStore()
+  useEffect(() => renderBreadcrumbsIntoKthHeader(language))
   return (
     <Row style={{ padding: '30px 0' }}>
       <MainMenu />
