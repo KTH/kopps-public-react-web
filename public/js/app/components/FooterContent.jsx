7e2a42ed9c9c119d97d0457445c3608691ff8d8a
Elena Rakhimova <elenara@kth.se>
2021-04-15 11:33:14 +0200

    feat: FooterContent component

---
+++ public/js/app/components/FooterContent.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useStore } from '../mobx'
+import i18n from '../../../../i18n'
+import { centralStudyCounselingUrl, koppsEmail, programmeLink } from '../util/links'
+import Footer from './Footer'
+
+function FooterContent() {
+  const { languageIndex } = useStore()
+  const { contentContact, centralStudyCounseling, applicationContact, koppsEmailText } = i18n.messages[
+    languageIndex
+  ].footerContent
+
+  const centralStudyCounselingAddress = centralStudyCounselingUrl()
+  const koppsEmailAddress = koppsEmail()
+
+  return (
+    <Footer>
+      <address>
+        {`${contentContact}: `}
+        <a href={centralStudyCounselingAddress}>{centralStudyCounseling}</a>
+        <br />
+        {`${applicationContact}: `}
+        <a href={koppsEmailAddress}>{koppsEmailText}</a>
+      </address>
+    </Footer>
+  )
+}
+
+export default FooterContent
