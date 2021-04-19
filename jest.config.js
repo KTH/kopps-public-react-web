9e78f3f646b4a4e4d39cd280746847e8aa50b8c5
Elena Rakhimova <elenara@kth.se>
2021-04-19 16:57:55 +0200

    feat: jest config for components

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  globals: {
+    NODE_ENV: 'test',
+  },
+  clearMocks: true,
+  notifyMode: 'failure-change',
+  transformIgnorePatterns: ['node_modules/(?!(@kth|@babel|@jest)/)'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['jest-extended'],
+  verbose: true,
+}
