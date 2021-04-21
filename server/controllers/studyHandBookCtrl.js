b67b511c2a5b9b71e2bf4e64729796612046a5e0
Elena Rakhimova <elenara@kth.se>
2021-04-21 16:22:05 +0200

    refactor: change study handbook ctrl name

---
+++ server/controllers/studyHandBookCtrl.js
@@ -0,0 +1,43 @@
+// @ts-check
+
+const log = require('kth-node-log')
+const language = require('kth-node-web-common/lib/language')
+
+const { browser: browserConfig, server: serverConfig } = require('../configuration')
+const i18n = require('../../i18n')
+const koppsApi = require('../kopps/koppsApi')
+
+const { getServerSideFunctions } = require('../utils/serverSideRendering')
+
+async function getStudyBook(req, res, next) {
+  try {
+    const lang = language.getLanguage(res)
+
+    const { createStore, getCompressedStoreCode, renderStaticPage } = getServerSideFunctions()
+
+    const applicationStore = createStore()
+    applicationStore.setLanguage(lang)
+    applicationStore.setBrowserConfig(browserConfig)
+    const compressedStoreCode = getCompressedStoreCode(applicationStore)
+
+    const { uri: proxyPrefix } = serverConfig.proxyPrefixPath
+    const html = renderStaticPage({ applicationStore, location: req.url, basename: proxyPrefix })
+    const title = i18n.message('site_name', lang)
+
+    res.render('app/index', {
+      html,
+      title,
+      compressedStoreCode,
+      description: title,
+      lang,
+      proxyPrefix,
+    })
+  } catch (err) {
+    log.error('Error in getStudyBook', { error: err })
+    next(err)
+  }
+}
+
+module.exports = {
+  getStudyBook,
+}
