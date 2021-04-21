2462a152353d46a8ddb3620c7d3b0cc3b7d30fa0
Elena Rakhimova <elenara@kth.se>
2021-04-21 16:24:12 +0200

    refactor: move common functions for schools comparison

---
+++ server/utils/schools.js
@@ -0,0 +1,47 @@
+const _deprecatedSchools = {
+  sv: [
+    'Datavetenskap och kommunikation',
+    'Elektro- och systemteknik',
+    'Informations- och kommunikationsteknik',
+    'Kemivetenskap',
+    'Teknik och hälsa',
+    'Teknikvetenskaplig kommunikation och lärande',
+  ],
+  en: [
+    'Chemical Science and Engineering',
+    'Computer Science and Communication',
+    'Education and Communication in Engineering Science',
+    'Electrical Engineering',
+    'Information and Communication Technology',
+    'Technology and Health',
+  ],
+}
+
+function _compareSchools(a, b) {
+  if (a.name < b.name) {
+    return -1
+  }
+  if (a.name > b.name) {
+    return 1
+  }
+  return 0
+}
+
+function _filterOutDeprecatedSchools(schoolsWithDepartments, lang) {
+  const deprecatedSchoolsWithDepartments = schoolsWithDepartments.filter(school =>
+    _deprecatedSchools[lang].includes(school.name)
+  )
+  const currentSchoolsWithDepartments = schoolsWithDepartments.filter(
+    school => !_deprecatedSchools[lang].includes(school.name)
+  )
+  return {
+    deprecatedSchoolsWithDepartments,
+    currentSchoolsWithDepartments,
+  }
+}
+
+module.exports = {
+  _deprecatedSchools,
+  _compareSchools,
+  _filterOutDeprecatedSchools,
+}
