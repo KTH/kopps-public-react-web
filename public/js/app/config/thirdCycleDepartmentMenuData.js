29c09104342b74a6b38d4653d85922bb6a630ddc
Elena Rakhimova <elenara@kth.se>
2021-04-22 18:04:39 +0200

    feat: add menu data for department courses page for third cycle study

---
+++ public/js/app/config/thirdCycleDepartmentMenuData.js
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+const i18n = require('../../../../i18n')
+const translate = require('../util/translate')
+const { pageLink } = require('../util/links')
+
+function getThirdCycleDepartmentMenuData(language, proxyPrefixPath, departmentName) {
+  const t = translate(i18n, language)
+  return {
+    ariaLabel: t('main_menu_aria_label'),
+    parentLink: {
+      text: t('department_courses'),
+      url: pageLink(proxyPrefixPath, 'utbildning/forskarutbildning/kurser', language),
+    },
+    navList: {
+      type: 'expandable',
+      items: [
+        {
+          id: 'directory',
+          type: 'ancestor',
+          text: departmentName,
+        },
+        {
+          id: 'courses',
+          type: 'leaf',
+          text: t('department_courses'),
+        },
+      ],
+    },
+  }
+}
+
+export default getThirdCycleDepartmentMenuData
