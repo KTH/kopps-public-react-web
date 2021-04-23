bfd5df1a3735da90cd05f504543cda897ae7905d
Elena Rakhimova <elenara@kth.se>
2021-04-23 17:04:32 +0200

    fix: refactor filter function and add test to it

--- server/controllers/thirdCycleStudyCtrl.js
+++ server/controllers/thirdCycleStudyCtrl.js
@@ -65,6 +65,14 @@ function departmentLink(proxyPrefixPath, departmentCode, lang) {
   return `${proxyPrefixPath}/utbildning/forskarutbildning/kurser/org/${departmentCode}${languageParam}`
 }
 
+function getOnlyThirdCycleCourses(courses, lang) {
+  const THIRD_CYCLE_LEVEL = {
+    en: 'Third cycle',
+    sv: 'Forskarnivå',
+  }
+  return courses.filter(course => course.level === THIRD_CYCLE_LEVEL[lang])
+}
+
 async function _fillCoursesApplicationStoreOnServerSide({ applicationStore, lang, departmentCode }) {
   applicationStore.setLanguage(lang)
   applicationStore.setBrowserConfig(browserConfig)
@@ -72,7 +80,7 @@ async function _fillCoursesApplicationStoreOnServerSide({ applicationStore, lang
   const departmentCourses = await koppsApi.getCourses({ departmentCode, lang })
   const { department: departmentName, courses } = departmentCourses
   applicationStore.setDepartmentName(departmentName)
-  applicationStore.setDepartmentCourses(courses.filter(course => course.level === 'Forskarnivå'))
+  applicationStore.setDepartmentCourses(getOnlyThirdCycleCourses(courses, lang))
 
   const departmentBreadCrumbItem = {
     url: departmentLink(browserConfig.proxyPrefixPath.uri, departmentCode, lang),
@@ -114,4 +122,5 @@ async function getCoursesPerDepartment(req, res, next) {
 module.exports = {
   getAllSchoolsAndDepartments,
   getCoursesPerDepartment,
+  getOnlyThirdCycleCourses,
 }
