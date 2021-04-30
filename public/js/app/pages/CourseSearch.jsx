4aced95a30d67c6021a1d6cb0a770eed14c22295
Elena Rakhimova <elenara@kth.se>
2021-04-30 17:52:20 +0200

    feat: make search page for third cycle courses

--- public/js/app/pages/CourseSearch.jsx
+++ public/js/app/pages/CourseSearch.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { observer } from 'mobx-react'
 import { Col, Row } from 'reactstrap'
 import { CollapseDetails } from '@kth/kth-kip-style-react-components'
 import { useStore } from '../mobx'
 import i18n from '../../../../i18n'
-
+import { PageHeading, Heading } from '@kth/kth-reactstrap/dist/components/studinfo'
+import Lead from '../components/Lead'
 import { SearchInputField, SearchResultDisplay } from '../components/index'
 
 const Start = () => {
   const { language: lang } = useStore()
-
+  const [pattern, setPattern] = useState('')
   const helptexts = [
     'koppspublic_search_help_1',
     'koppspublic_search_help_2',
@@ -21,12 +22,20 @@ const Start = () => {
     'koppspublic_search_help_9',
   ].map(s => i18n.message(s, lang))
 
+  function handleSubmit(pattern) {
+    // todo: useContext instead of props drilling
+    // or useStore for reloadiing page with urls props
+    setPattern(pattern)
+  }
+
   return (
     <main id="mainContent">
       <Row>
         <Col>
-          <h1>Sök kurs</h1>
-          <p>{i18n.message('koppspublic_search_introduction', lang)}</p>
+          {/* <h1>Sök kurs</h1> */}
+          <PageHeading>Sök forskarkurs</PageHeading>
+          <Lead text="Här kan du söka bland KTHs forskarutbildningskurser." />
+          {/* <p>{i18n.message('koppspublic_search_introduction', lang)}</p> */}
           <CollapseDetails color="white" title={i18n.message('koppspublic_search_help_h', lang)}>
             <ul>
               {helptexts.map(value => (
@@ -35,9 +44,13 @@ const Start = () => {
             </ul>
           </CollapseDetails>
 
-          <SearchInputField caption="Sök" />
+          <SearchInputField caption="Sök" pattern={pattern} onSubmit={handleSubmit} />
+          {/* research only link */}
+          <a href="/student/kurser/sokkurs">
+            På sidan Sök kurs kan du med hjälp av sökkriterier söka bland KTHs samtliga kurser inklusive forskarkurser.
+          </a>
 
-          <SearchResultDisplay />
+          <SearchResultDisplay pattern={pattern} />
         </Col>
       </Row>
     </main>
