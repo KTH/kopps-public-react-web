b5e881c014b0ea142ce9384f138c202d0160b81a
Elena Rakhimova <elenara@kth.se>
2021-05-24 17:04:59 +0200

    refactor: move useAsync hook to a separate file

--- public/js/app/components/SearchResultDisplay.jsx
+++ public/js/app/components/SearchResultDisplay.jsx
@@ -13,6 +13,7 @@ import i18n from '../../../../i18n'
 import { stringifyUrlParams, CLIENT_EDU_LEVELS, CLIENT_SHOW_OPTIONS } from '../../../../domain/searchParams'
 // eslint-disable-next-line import/no-cycle
 import { SearchAlert } from './index'
+import { STATUS, ERROR_ASYNC, useAsync } from '../hooks/searchUseAsync'
 
 function _getThisHost(thisHostBaseUrl) {
   return thisHostBaseUrl.slice(-1) === '/' ? thisHostBaseUrl.slice(0, -1) : thisHostBaseUrl
@@ -37,73 +38,6 @@ async function koppsCourseSearch(language, proxyUrl, params) {
     }
     throw error
   }
-}
-const STATUS = {
-  pending: 'pending',
-  resolved: 'resolved',
-  overflow: 'overflow',
-  noQueryProvided: 'noQueryProvided',
-  noHits: 'noHits',
-  rejected: 'rejected',
-}
-const ERROR_ASYNC = {
-  overflow: 'errorOverflow',
-  noQueryProvided: 'noQueryProvided',
-  noHits: 'errorEmpty',
-  rejected: 'errorUnknown',
-}
-
-function asyncReducer(state, action) {
-  switch (action.type) {
-    case 'pending': {
-      return { status: STATUS.pending, data: null, error: null }
-    }
-    case 'resolved': {
-      return { status: STATUS.resolved, data: action.data, error: null }
-    }
-    case 'overflow': {
-      return { status: STATUS.overflow, data: null, error: ERROR_ASYNC.overflow }
-    }
-    case 'noQueryProvided': {
-      return { status: STATUS.noQueryProvided, data: null, error: ERROR_ASYNC.noQueryProvided }
-    }
-    case 'noHits': {
-      return { status: STATUS.noHits, data: null, error: ERROR_ASYNC.noHits }
-    }
-    case 'rejected': {
-      return { status: STATUS.rejected, data: null, error: ERROR_ASYNC.rejected } // for debug use: action.error
-    }
-    default: {
-      throw new Error(`Unhandled action type: ${action.type}`)
-    }
-  }
-}
-
-function useAsync(asyncCallback, initialState) {
-  const [state, dispatch] = React.useReducer(asyncReducer, {
-    status: STATUS.idle,
-    data: null,
-    error: null,
-    ...initialState,
-  })
-  useEffect(() => {
-    const promise = asyncCallback()
-    if (!promise) return
-    dispatch({ type: STATUS.pending })
-    promise.then(
-      data => {
-        const { searchHits, errorCode } = data
-        if (errorCode && errorCode === 'search-error-overflow') dispatch({ type: STATUS.overflow })
-        else if (searchHits && searchHits.length === 0) dispatch({ type: STATUS.noHits })
-        else if (!searchHits && data === 'No query restriction was specified')
-          dispatch({ type: STATUS.noQueryProvided })
-        else dispatch({ type: STATUS.resolved, data })
-      },
-      error => dispatch({ type: STATUS.rejected, error }) // 'search-error-unknown'
-    )
-  }, [asyncCallback])
-
-  return state
 }
 
 function renderAlertToTop(errorType, languageIndex) {
