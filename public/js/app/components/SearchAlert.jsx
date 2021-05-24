ee285685689bcf98c659d113b6af94368bf104e0
Elena Rakhimova <elenara@kth.se>
2021-05-24 17:09:53 +0200

    refactor: use ERROR_ASYNC

--- public/js/app/components/SearchAlert.jsx
+++ public/js/app/components/SearchAlert.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Alert } from '@kth/kth-reactstrap/dist/components/studinfo'
 import PropTypes from 'prop-types'
 import i18n from '../../../../i18n'
+import { ERROR_ASYNC } from '../hooks/searchUseAsync'
 
 const SearchAlert = ({ alertType: externalAlertType, languageIndex }) => {
   const { searchAlarms } = i18n.messages[languageIndex]
@@ -17,7 +18,7 @@ const SearchAlert = ({ alertType: externalAlertType, languageIndex }) => {
 }
 
 SearchAlert.propTypes = {
-  alertType: PropTypes.oneOf(['errorOverflow', 'noQueryProvided', 'errorEmpty', 'errorUnknown', null]).isRequired,
+  alertType: PropTypes.oneOf([...Object.values(ERROR_ASYNC), null]).isRequired,
   languageIndex: PropTypes.oneOf([0, 1]).isRequired,
 }
 
