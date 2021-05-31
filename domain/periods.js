0c5617c4a27cc1cce0e106829bdc4f9d0b4ad119
Rickard Falk <rickard.falk@gmail.com>
2021-05-31 07:38:07 +0200

    feat(kp-127): add periods to domain

---
+++ domain/periods.js
@@ -0,0 +1,17 @@
+const KthPeriod = {
+  P0: 0,
+  P1: 1,
+  P2: 2,
+  P3: 3,
+  P4: 4,
+  P5: 5,
+}
+
+const ALL_PERIODS = [KthPeriod.P0, KthPeriod.P1, KthPeriod.P2, KthPeriod.P3, KthPeriod.P4, KthPeriod.P5]
+
+const ORDINARY_PERIODS = [KthPeriod.P1, KthPeriod.P2, KthPeriod.P3, KthPeriod.P4]
+
+module.exports = {
+  ALL_PERIODS,
+  ORDINARY_PERIODS,
+}
