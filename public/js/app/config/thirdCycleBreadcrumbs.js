b133aad288f9b3efd29b03c2c1dbddee67558e56
Elena Rakhimova <elenara@kth.se>
2021-12-14 16:07:50 +0100

    fix: use correct path for object variable, fix breadcrumbs path

--- public/js/app/config/thirdCycleBreadcrumbs.js
+++ public/js/app/config/thirdCycleBreadcrumbs.js
@@ -14,7 +14,7 @@ function getThirdCycleBreadcrumbs(applicationStore) {
       label: t('main_menu_third_cycle_studies'),
     },
     {
-      url: pageLink(browserConfig.proxyPrefixPath.thirdCycleRoot, language),
+      url: pageLink(browserConfig.redirectProxyPath.thirdCycleRoot, language),
       label: t('department_courses'),
     },
   ]
