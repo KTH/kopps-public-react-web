12a7ac2eaad259af161af9766749ff5c2666fbea
Elena Rakhimova <elenara@kth.se>
2021-12-21 16:13:30 +0100

    fix: update target in link and component library update

--- public/js/app/pages/Programme.jsx
+++ public/js/app/pages/Programme.jsx
@@ -22,7 +22,7 @@ function ProgrammeTermsLinkListItem({ term }) {
   const { language, programmeCode, lengthInStudyYears } = useStore()
   const studyYear = calculateStudyYear(term, lengthInStudyYears)
   return (
-    <Link key={term} href={programTermLink(programmeCode, term, `arskurs${studyYear}`, language)}>
+    <Link key={term} href={programTermLink(programmeCode, term, `arskurs${studyYear}`, language)} target="_blank">
       {programmeTermLinkText(term)}
     </Link>
   )
