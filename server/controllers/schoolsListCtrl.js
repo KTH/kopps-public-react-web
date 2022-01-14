8733b924cb86395eef449ded732ec41f1197e10c
Elena Rakhimova <elenara@kth.se>
2022-01-14 13:37:57 +0100

    refactor: move out schools list store functions

--- server/controllers/schoolsListCtrl.js
+++ server/controllers/schoolsListCtrl.js
@@ -3,28 +3,20 @@
 const log = require('kth-node-log')
 const language = require('kth-node-web-common/lib/language')
 
-const { browser: browserConfig, server: serverConfig } = require('../configuration')
+const { server: serverConfig } = require('../configuration')
 const i18n = require('../../i18n')
 
 const koppsApi = require('../kopps/koppsApi')
 
 const { getServerSideFunctions } = require('../utils/serverSideRendering')
-const { compareSchools, filterOutDeprecatedSchools } = require('../../domain/schools')
+const { fillStoreWithBasicConfig, fetchAndFillSchoolsList } = require('../stores/schoolsListStoreSSR')
 
 async function _fillApplicationStoreOnServerSide({ applicationStore, lang }) {
-  applicationStore.setLanguage(lang)
-  applicationStore.setBrowserConfig(browserConfig)
+  fillStoreWithBasicConfig({ applicationStore, lang })
+
   const listForActiveCourses = true
   const params = { departmentCriteria: koppsApi.DEPARTMENT_CRITERIA.HAS_COURSES, listForActiveCourses, lang }
-  const schoolsWithDepartments = await koppsApi.listSchoolsWithDepartments(params)
-  const { currentSchoolsWithDepartments, deprecatedSchoolsWithDepartments } = filterOutDeprecatedSchools(
-    schoolsWithDepartments,
-    lang
-  )
-  deprecatedSchoolsWithDepartments.sort(compareSchools)
-  currentSchoolsWithDepartments.sort(compareSchools)
-  applicationStore.setCurrentSchoolsWithDepartments(currentSchoolsWithDepartments)
-  applicationStore.setDeprecatedSchoolsWithDepartments(deprecatedSchoolsWithDepartments)
+  await fetchAndFillSchoolsList(applicationStore, params)
 }
 
 async function getSchoolsList(req, res, next) {
@@ -32,10 +24,14 @@ async function getSchoolsList(req, res, next) {
     const lang = language.getLanguage(res)
 
     const { createStore, getCompressedStoreCode, renderStaticPage } = getServerSideFunctions()
+    log.info(`Creating a default application store for schools with all-type courses`)
 
     const applicationStore = createStore()
+    log.debug(`Starting to fill a default application store with all schools with any type of courses`)
+
     await _fillApplicationStoreOnServerSide({ applicationStore, lang })
     const compressedStoreCode = getCompressedStoreCode(applicationStore)
+    log.info(`Default store was filled in and compressed on server side, for schools with all-type courses`)
 
     const { schoolsList: proxyPrefix } = serverConfig.proxyPrefixPath
     const html = renderStaticPage({ applicationStore, location: req.url, basename: proxyPrefix })
---
+++ server/stores/schoolsListStoreSSR.js
@@ -0,0 +1,61 @@
+const log = require('kth-node-log')
+
+const { browser: browserConfig } = require('../configuration')
+const i18n = require('../../i18n')
+const koppsApi = require('../kopps/koppsApi')
+const { compareSchools, filterOutDeprecatedSchools } = require('../../domain/schools')
+
+// @ts-check
+
+module.exports = {
+  fillStoreWithBasicConfig,
+  fetchAndFillSchoolsList,
+}
+/**
+ * add props to a MobX-stores on server side
+ * list of shools with departments
+ * schools with any type of courses -> student/kurser/org/
+ * schools which has third-cycle courses -> utbildning/forskarutbildning/kurser/avdelning
+ * so that its data can be used with the useStore() hook on client side
+ *
+ */
+
+/**
+ * @param {object} options.applicationStore
+ * @param {string} options.lang
+ */
+function fillStoreWithBasicConfig({ applicationStore, lang }) {
+  applicationStore.setLanguage(lang)
+  applicationStore.setBrowserConfig(browserConfig)
+}
+
+/**
+ * @param {object} applicationStore
+ * @param {string} params.departmentCriteria
+ * @param {array} params.listForActiveCourses
+ * @param {string} params.lang
+ * @returns {object}
+ */
+async function fetchAndFillSchoolsList(applicationStore, params) {
+  const { departmentCriteria, lang } = params
+  log.info('Fetching schools with departments', { departmentCriteria: params.departmentCriteria })
+
+  const { schoolsWithDepartments, statusCode } = await koppsApi.listSchoolsWithDepartments(params)
+  applicationStore.setStatusCode(statusCode)
+  if (statusCode !== 200) {
+    log.debug('Failed to fetch schools', { departmentCriteria })
+    return
+  }
+  log.info('Successfully fetched department courses', { departmentCriteria })
+
+  const { currentSchoolsWithDepartments, deprecatedSchoolsWithDepartments } = filterOutDeprecatedSchools(
+    schoolsWithDepartments,
+    lang
+  )
+  deprecatedSchoolsWithDepartments.sort(compareSchools)
+  currentSchoolsWithDepartments.sort(compareSchools)
+  applicationStore.setCurrentSchoolsWithDepartments(currentSchoolsWithDepartments)
+  applicationStore.setDeprecatedSchoolsWithDepartments(deprecatedSchoolsWithDepartments)
+
+  return schoolsWithDepartments
+}
