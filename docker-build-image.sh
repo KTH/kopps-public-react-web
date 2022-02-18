df60daa79cc02da2cb75ceaaabaa5e563e9261b1
Elena Rakhimova <elenara@kth.se>
2022-02-18 15:37:16 +0100

    refactor: docker commands

--- docker-build-image.sh
+++ docker-build-image.sh
@@ -9,13 +9,10 @@ function echoYellow() {
 
 echo
 echoYellow "|--------------------------------------------------------|"
-echoYellow "| Building the application with Docker for dev env       |"   
+echoYellow "|    Building the Docker image for development env       |"   
 echoYellow "|--------------------------------------------------------|\n"
 
 IMAGE_NAME="kopps-prw"
-OUTSIDE_HOST_PORT="3000"
-INSIDE_CONTAINER_PORT="3000"
-
 
 if [ "$ENV" == "dev" ]; then
   echo
@@ -25,8 +22,4 @@ if [ "$ENV" == "dev" ]; then
   echo
   echoYellow "  2. List images\n"
   docker images
-
-  echo
-  echoYellow "  2. Running start image on client app, [host port]:[container port]\n"
-  docker run --env-file .env -p "$OUTSIDE_HOST_PORT":"$INSIDE_CONTAINER_PORT" "$IMAGE_NAME"
 fi
