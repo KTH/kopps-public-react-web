86af5505083634ec38f829e03580cbec5c72661b
Elena Rakhimova <elenara@kth.se>
2022-02-24 15:52:04 +0100

    clean: remove commented code

--- public/js/app/config/mocks/mockCommonSettings.js
+++ public/js/app/config/mocks/mockCommonSettings.js
@@ -1,32 +1,5 @@
 import config from '../../../../../config/commonSettings'
-// const studentRoot = '/student/kurser'
-// const thirdCycleRoot = '/utbildning/forskarutbildning/kurser'
 
 const commonSettings = config
 
-// const commonSettings = {
-//   // The proxy prefix path if the application is proxied. E.g /places
-//   proxyPrefixPath: {
-//     uri: studentRoot,
-//     courseSearch: `${studentRoot}/sokkurs`,
-//     courseSearchInternApi: `${studentRoot}/intern-api/sok`,
-//     department: `${studentRoot}/org`,
-//     programme: `${studentRoot}/program`,
-//     programmesList: `${studentRoot}/kurser-inom-program`,
-//     schoolsList: `${studentRoot}/program`,
-//     studyHandbook: '/student/program/shb',
-//     thirdCycleCourseSearch: `${thirdCycleRoot}/sok`,
-//     thirdCycleSchoolsAndDepartments: `${thirdCycleRoot}/avdelning`,
-//     thirdCycleCoursesPerDepartment: `${thirdCycleRoot}/org`,
-//     literatureList: `${studentRoot}/lit`,
-//   },
-//   // Don't contain any content
-//   // is just immedately redirected.
-//   redirectProxyPath: {
-//     studentRoot,
-//     thirdCycleRoot,
-//     coursesPerDepartment: `${studentRoot}/kurser-per-avdelning`,
-//     departmentCourses: `${studentRoot}/avdelning/:departmentCode/kurser/`,
-//   },
-// }
 export default commonSettings
