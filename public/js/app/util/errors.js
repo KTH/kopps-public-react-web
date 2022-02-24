cff1793d6177e679f4021287a841cb70a7620ee5
Elena Rakhimova <elenara@kth.se>
2022-02-24 15:51:32 +0100

    fix: functions to check if store contains browserConfig and proxyPrefixPath

---
+++ public/js/app/util/errors.js
@@ -0,0 +1,10 @@
+function throwErrorIfNoBrowserConfig(browserConfig) {
+  if (!browserConfig)
+    throw new TypeError(
+      `Error while generating meny or breadcrumbs: browserConfig is not defined, check if store was passed in or it was filled in on server-side`
+    )
+  if (!browserConfig.proxyPrefixPath)
+    throw new TypeError('Error while generating meny or breadcrumbs: proxyPrefixPath is not defined in browserConfig')
+}
+
+export { throwErrorIfNoBrowserConfig }
