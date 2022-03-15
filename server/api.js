6bef093ccd5c2123b8200b271867e7f5a7d6ba7c
Elena Rakhimova <elenara@kth.se>
2022-03-15 17:46:58 +0100

    fix: remoe double connection to kopps-api

--- server/api.js
+++ server/api.js
@@ -5,22 +5,31 @@ const redis = require('kth-node-redis')
 const connections = require('@kth/api-call').Connections
 const config = require('./configuration').server
 
-const koppsOpts = {
+// const koppsOpts = {
+//   log,
+//   https: true,
+//   redis,
+//   cache: config.cache,
+//   timeout: 5000,
+//   defaultTimeout: config.koppsApi.defaultTimeout,
+//   retryOnESOCKETTIMEDOUT: true,
+//   useApiKey: false, // performs api-key checks against the apis, if a "required" check fails, the app will exit. Required apis are specified in the config
+// }
+const opts = {
   log,
-  https: true,
   redis,
-  cache: config.cache,
-  timeout: 5000,
-  defaultTimeout: config.koppsApi.defaultTimeout,
-  retryOnESOCKETTIMEDOUT: true,
-  useApiKey: false, // performs api-key checks against the apis, if a "required" check fails, the app will exit. Required apis are specified in the config
+  timeout: 30000,
+  retryOnESOCKETTIMEDOUT: false,
+  useApiKey: false,
+  checkAPIs: false, // performs api-key checks against the apis, if a "required" check fails, the app will exit. Required apis are specified in the config
 }
 
-config.koppsApi.doNotCallPathsEndpoint = true // skip checking _paths, because kopps doesnt have it
-config.koppsApi.connected = true
+// config.koppsApi.doNotCallPathsEndpoint = true // skip checking _paths, because kopps doesnt have it
+// config.koppsApi.connected = true
 
-const koppsConfig = {
-  koppsApi: config.koppsApi,
-}
+// const koppsConfig = {
+//   koppsApi: config.koppsApi,
+// }
 
-module.exports = connections.setup(koppsConfig, koppsConfig, koppsOpts)
+// module.exports = connections.setup(koppsConfig, koppsConfig, koppsOpts)
+module.exports = connections.setup(config.nodeApi, config.apiKey, opts)
