6afb05c55e2d7c375f3fc365d29ce88a0feba835
Rao-Sabahat-Ali <raosabahat01@live.com>
2022-06-28 20:22:51 +0200

    fix: replaced # with complete url

--- public/js/app/util/stringUtil.js
+++ public/js/app/util/stringUtil.js
@@ -1,16 +1,20 @@
 function replacePathNameWithHref(element) {
-  /* const aEl = element.getElementsByTagName('a')
+  const aEl = element.getElementsByTagName('a')
   for (let i = 0; i < aEl.length; i++) {
     const a = aEl[i]
     if (!String(a.outerHTML).includes(a.href)) {
-      a.outerHTML = a.outerHTML.replace(a.pathname, a.href)
+      if (a.outerHTML.includes('#')) {
+        a.outerHTML = a.outerHTML.replace('#', a.href)
+      } else {
+        a.outerHTML = a.outerHTML.replace(a.pathname, a.href)
+      }
     }
-  } */
+  }
 }
 
-function getCurrentHost(thisHostBaseUrl) {
+function getCurrentHost(thisHostBaseUrl, replaceAppWithWWW = true) {
   let hostURL = thisHostBaseUrl
-  if (origin.includes('app-')) {
+  if (origin.includes('app-') && replaceAppWithWWW) {
     hostURL = String(thisHostBaseUrl).replace('app-', 'www-')
   }
   return hostURL.slice(-1) === '/' ? hostURL.slice(0, -1) : hostURL
