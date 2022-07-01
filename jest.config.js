5573408e3ed35a4e0b17583a3f42ec4cddafcad3
Elena Rakhimova <elenara@kth.se>
2022-07-01 16:35:10 +0200

    test: ignore uuid package export

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   },
   clearMocks: true,
   notifyMode: 'failure-change',
-  transformIgnorePatterns: ['node_modules/(?!(@kth|@babel|@jest)/)'],
+  transformIgnorePatterns: ['node_modules/(?!(@kth|@babel|@jest|uuid|nanoid)/)'],
   moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['jest-extended/all'],
   testEnvironment: 'jsdom',
