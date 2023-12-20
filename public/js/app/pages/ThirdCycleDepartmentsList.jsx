f9cc081bea9702577267f5d7692e2b468a54e2c4
victorsd <victorsd@kth.se>
2023-12-20 14:49:08 +0100

    cleanup(KP-297): removed unnecessary import

--- public/js/app/pages/ThirdCycleDepartmentsList.jsx
+++ public/js/app/pages/ThirdCycleDepartmentsList.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { Col, Row } from 'reactstrap'
-import { PageHeading, Heading, LinkList, Link, Text } from '@kth/kth-reactstrap/dist/components/studinfo'
+import { PageHeading, Heading, LinkList, Link } from '@kth/kth-reactstrap/dist/components/studinfo'
 import { CollapseDetails } from '@kth/kth-reactstrap/dist/components/utbildningsinfo'
 
 import Article from '../components/Article'
