9cb0fa88a11a4a35f707fa06ea0fc9c8e7385608
Karl Andin <karl.andin@drakryggen.com>
2024-04-12 17:23:10 +0200

    init mobile menu panel from compoennt (and remove from publicLayout)

--- server/server.js
+++ server/server.js
@@ -119,7 +119,6 @@ server.use(_addProxy('/static/browserConfig'), browserConfigHandler)
 // Files/statics routes
 server.use(_addProxy('/static/kth-style'), express.static('./node_modules/kth-style/dist', staticOption))
 server.use(_addProxy('/assets'), express.static('./node_modules/@kth/style/assets', staticOption))
-server.use(_addProxy('/assets/js'), express.static('./node_modules/@kth/style/dist/esm', staticOption))
 
 // Map static content like images, css and js.
 server.use(_addProxy('/static'), express.static('./dist', staticOption))
