8522de1dab8d87eb66d5f3172ba333f360a89e61
amirhossein-haerian <ahae@kth.se>
2024-05-29 18:10:55 +0200

    refactor(issues/KP-308-search-third-cycle-courses): jest snapshot has been created for the new changes also mor functionalities has been added to HelpText component

--- public/js/app/components/HelpTexts.jsx
+++ public/js/app/components/HelpTexts.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
 
-function HelpTexts({ helptexts }) {
+/**
+ * HelpTexts Component
+ *
+ * This component displays a list of help texts. Some of these texts may contain HTML elements
+ * that need to be rendered as HTML rather than plain text. To specify which texts should be
+ * rendered as HTML, pass their indexes in the `htmlIndexes` prop. This ensures that only the
+ * specified texts are treated as HTML.
+ *
+ * Props:
+ * - helptexts (array): An array of help text strings. Some strings may contain HTML.
+ * - htmlIndexes (array): An array of indexes indicating which help texts should be rendered
+ *   as HTML.
+ *
+ * Example usage:
+ * const helptexts = [
+ *   'This is a plain text help message.',
+ *   'If you have questions, please contact <a href="mailto:kopps@kth.se">kopps@kth.se</a>.',
+ *   'Another plain text message.'
+ * ];
+ * const htmlIndexes = [1];
+ *
+ * <HelpTexts helptexts={helptexts} htmlIndexes={htmlIndexes} />
+ */
+
+function HelpTexts({ helptexts, htmlIndexes = [] }) {
   return (
     <div className="article">
       <ul>
-        {helptexts.map(value => (
-          <li key={value}>{value}</li>
-        ))}
+        {helptexts.map((value, index) => {
+          if (htmlIndexes.includes(index)) {
+            return (
+              // eslint-disable-next-line react/no-danger
+              <li key={value} dangerouslySetInnerHTML={{ __html: value }} />
+            )
+          } else return <li key={value}>{value}</li>
+        })}
       </ul>
     </div>
   )
