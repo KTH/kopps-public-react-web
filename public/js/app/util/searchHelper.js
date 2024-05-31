800d4566006ba51c09ae8de1031f6d1460755495
amirhossein-haerian <ahae@kth.se>
2024-05-31 09:55:27 +0200

    refactor(issues/KP-308-search-third-cycle-courses): add some comments on getHelpText function for better understanding

--- public/js/app/util/searchHelper.js
+++ public/js/app/util/searchHelper.js
@@ -1,10 +1,17 @@
 import i18n from '../../../../i18n'
 
 function getHelpText(langIndex, nameOfInstruction, instructionKeys) {
+  /**
+   * Retrieves a list of translated instructional texts based on the given language index,
+   * the name of the instruction set, and the specific instruction keys.
+   */
+
   const messages = i18n.messages[langIndex]
   const instructions = messages[nameOfInstruction]
+  // instructions is an object containing all the instructions for the specified language and instruction set.
 
   const instructionsTexts = instructionKeys.map(s => instructions[s])
+  // instructionsTexts is the list of translated instructions based on the provided instructionKeys.
 
   return instructionsTexts
 }
