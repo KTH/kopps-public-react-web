76d0b574c7e77cf72fa7a76e5761937a4ee942ab
Axel Björkqvist <axelbjo@kth.se>
2024-06-26 15:06:13 +0200

    Removed junk

--- public/js/app/components/Article.jsx
+++ public/js/app/components/Article.jsx
@@ -2,21 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import TypescriptComponent from './TypescriptComponent.tsx'
 
-// function Article({ classNames, children, uiKey = '' }) {
-//   return (
-//     <article className={`article article--no-start-margin ${classNames ? classNames.join(' ') : ''}`} key={uiKey}>
-//       {children}
-//     </article>
-//   )
-// }
 function Article({ classNames, children, uiKey = '' }) {
   return (
-    <div>
-      <TypescriptComponent title={"I'M A TYPESCRIPT COMPONENT"} />
-      <article className={`article article--no-start-margin ${classNames ? classNames.join(' ') : ''}`} key={uiKey}>
-        {children}
-      </article>
-    </div>
+    <article className={`article article--no-start-margin ${classNames ? classNames.join(' ') : ''}`} key={uiKey}>
+      {children}
+    </article>
   )
 }
 
--- public/js/app/util/getSomeValue.ts
+++
@@ -1,5 +0,0 @@
-const getSomeValue = (nr: number) => {
-  return nr * 2
-}
-
-export default getSomeValue
