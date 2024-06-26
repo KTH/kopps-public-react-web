0e2b977734c7fae2f61edb2f1cd6676e2767a3ce
Axel Björkqvist <axelbjo@kth.se>
2024-06-26 15:06:13 +0200

    Removed junk

--- public/js/app/util/getSomeValue.test.ts
+++
@@ -1,8 +0,0 @@
-import getSomeValue from './getSomeValue'
-
-describe('Test getSomeValue function', () => {
-  test('It should multiply a number by two', () => {
-    const nr = 5
-    expect(getSomeValue(5)).toEqual(10)
-  })
-})
