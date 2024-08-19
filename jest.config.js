6bd47bb7d5ca071b407bab158d216d09a1f89787
amirhossein-haerian <ahae@kth.se>
2024-08-19 13:58:37 +0200

    feat(KP-355): fix the issue for loading css/scss files inside jest

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,9 @@ module.exports = {
   notifyMode: 'failure-change',
   transformIgnorePatterns: ['node_modules/(?!(@kth|@babel|@jest|uuid|nanoid)/)'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+  },
   setupFilesAfterEnv: ['jest-extended/all'],
   testEnvironment: 'jsdom',
   verbose: true,
