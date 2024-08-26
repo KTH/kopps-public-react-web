6dfced56d69ce242813d3271d6f0d17119f917fd
amirhossein-haerian <ahae@kth.se>
2024-08-26 08:09:47 +0200

    feat(KP-362): some tiny omprovements has been made

--- public/js/app/components/SearchFilters/SearchFilters.test.tsx
+++ public/js/app/components/SearchFilters/SearchFilters.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen, fireEvent, within } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import SearchFilters from './index'
 import { FILTER_MODES } from './types'
