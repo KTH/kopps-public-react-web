12173bd17e8c7a31a2365146c5630c9dcaed1f39
amirhossein-haerian <ahae@kth.se>
2025-04-03 13:46:06 +0200

    ref(KP-386): change the names in ladokAPI.js

--- server/ladok/ladokApi.js
+++ server/ladok/ladokApi.js
@@ -11,20 +11,20 @@ async function searchCourses(pattern, lang) {
 
 async function getActiveProgramTillfalle(programCode, startPeriod, lang) {
   const client = createApiClient(serverConfig.ladokMellanlagerApi)
-  const courses = await client.getActiveProgramTillfalle(programCode, startPeriod, lang)
-  return courses
+  const program = await client.getActiveProgramTillfalle(programCode, startPeriod, lang)
+  return program
 }
 
 async function getProgramVersion(programCode, startPeriod, lang) {
   const client = createApiClient(serverConfig.ladokMellanlagerApi)
-  const courses = await client.getProgramVersion(programCode, startPeriod, lang)
-  return courses
+  const programVersion = await client.getProgramVersion(programCode, startPeriod, lang)
+  return programVersion
 }
 
 async function getProgramStructure(programCode, lang) {
   const client = createApiClient(serverConfig.ladokMellanlagerApi)
-  const courses = await client.getUtbildningstilfalleStructure(programCode, lang)
-  return courses
+  const programStructure = await client.getUtbildningstilfalleStructure(programCode, lang)
+  return programStructure
 }
 
 module.exports = {
