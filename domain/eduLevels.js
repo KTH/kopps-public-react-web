8b3f6e018c46309538accfed69d5f157867f65fb
Charlotta Jaunviksna <charlotta.jaunviksna@precisit.com>
2025-04-22 13:49:04 +0200

    feat(KUI-1472): refactor edu levels file

--- domain/eduLevels.js
+++ domain/eduLevels.js
@@ -1,16 +1,11 @@
-const PREPARATORY_EDU_LEVEL = 'PREPARATORY'
-const BASIC_EDU_LEVEL = 'BASIC'
-const ADVANCED_EDU_LEVEL = 'ADVANCED'
-const RESEARCH_EDU_LEVEL = 'RESEARCH'
-
-const CLIENT_EDU_LEVELS = ['FUPKURS', '2007GKURS', '2007AKURS', '2007FKURS']
-
 const EDUCATION_LEVEL_MAP = {
-  FUPKURS: PREPARATORY_EDU_LEVEL, // Education preparing for university studies
-  '2007GKURS': BASIC_EDU_LEVEL, // Studies at university
-  '2007AKURS': ADVANCED_EDU_LEVEL, // Doctoral studies
-  '2007FKURS': RESEARCH_EDU_LEVEL, // Post-doc studies
+  FUPKURS: 'PREPARATORY',
+  '2007GKURS': 'BASIC',
+  '2007AKURS': 'ADVANCED',
+  '2007FKURS': 'RESEARCH',
 }
+
+const CLIENT_EDU_LEVELS = Object.keys(EDUCATION_LEVEL_MAP)
 
 function educationalLevel(levelCode) {
   if (typeof levelCode !== 'string') {
@@ -20,9 +15,7 @@ function educationalLevel(levelCode) {
   const level = EDUCATION_LEVEL_MAP[levelCode]
 
   if (!level) {
-    throw new Error(
-      `Unknown education level code: ${levelCode}. Allowed values: ${Object.keys(EDUCATION_LEVEL_MAP).join(', ')}`
-    )
+    throw new Error(`Unknown education level code: ${levelCode}. Allowed values: ${CLIENT_EDU_LEVELS.join(', ')}`)
   }
 
   return level
